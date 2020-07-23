d9e6737c19aa46524d22514ea6d82e5ec5230df3
Saleem Saleh <salem1234saleem@gmail.com>
2020-07-23 20:22:17 +0000

    Automatically backed up by Learn

--- app/models/sighting.rb
+++ app/models/sighting.rb
@@ -3,7 +3,12 @@ class Sighting < ApplicationRecord
   belongs_to :location
 end
 
+def index
+  sightings = Sighting.all
+  render json: sightings.to_json(include: [:bird, :location])
+end
+ 
 def show
   sighting = Sighting.find_by(id: params[:id])
-  render json: sighting, include: [:bird, :location]
+  render json: sighting.to_json(include: [:bird, :location])
 end
